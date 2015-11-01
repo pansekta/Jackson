0b7c66964bca00d65fb13063cd3daca3c32e2606
pansekta <200703@student.pwr.wroc.pl>
2015-11-01 14:30:11 +0100

    gh

--- Jackson/Domain/ClientBusinessInfo.cs
+++ Jackson/Domain/ClientBusinessInfo.cs
@@ -7,7 +7,7 @@
     public virtual string LastName { get; set; }
     public virtual string Email { get; set; }
     public virtual string Phone { get; set; }
-    public virtual int YearlyIncome { get; set; }
+    public virtual decimal YearlyIncome { get; set; }
     public virtual string JobPosition { get; set; }
     public virtual string Company { get; set; }
     public virtual string PersonalData { get; set; }
